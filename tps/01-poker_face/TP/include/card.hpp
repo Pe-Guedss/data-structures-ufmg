c936f770a39ff88eb2b1ff3d06f5e10663423cbb
Pe-Guedss <pedro.og2002@gmail.com>
2022-05-20 19:21:23 -0300

    Changing card constructor.

--- tps/01-poker_face/TP/include/card.hpp
+++ tps/01-poker_face/TP/include/card.hpp
@@ -7,27 +7,29 @@
 #ifndef CARD_H
 #define CARD_H
 
+#include <string>
+
 /**
  * @brief Classe que armazena e gerencia as informacoes das cartas do baralho, enquanto objetos individuais.
  *
  */
-class Card {
-    public:
-        /**
-         * @brief Construtor padrao que inicializa todas as variaveis privadas da classe.
-         */
-        Card();
-        /**
-         * @brief Construtor que inicia os valores da carta como se deve.
-         * 
-         * @param number Número associado à carta (1 a 13)
-         * @param nipe Naipe associado à carta (O, E, C, P)
-         */
-        Card(const int& number, const char& nipe);
+class Card
+{
+public:
+    /**
+     * @brief Construtor padrao que inicializa todas as variaveis privadas da classe.
+     */
+    Card();
+    /**
+     * @brief Construtor que inicia os valores da carta como se deve.
+     *
+     * @param cardCdde Número associado à carta (1 a 13) juntamente com o naipe associado à carta (O, E, C, P)
+     */
+    Card(const std::string &cardCode);
 
-    private:
-        int number;
-        char nipe;
+private:
+    int number;
+    char nipe;
 };
 
 #endif
