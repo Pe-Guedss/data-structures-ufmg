51b5a5798d6fb29383fb5f06ad10d1c7599442b8
Pe-Guedss <pedro.og2002@gmail.com>
2022-05-24 19:33:31 -0300

    Implementing the insertion sort algorithm.

--- practices/04-number_frequency/main.cpp
+++ practices/04-number_frequency/main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 
+void swapElements(int *arr, int pos, int *element)
+{
+    int aux = -1;
+    aux = arr[pos];
+    arr[pos] = *element;
+    *element = aux;
+}
+
+void orderArr(int *arr, int n)
+{
+    int i = 0, j = 0;
+    while (i < n)
+    {
+        int minimumValue = arr[i];
+        for (j = i; j < n; j++)
+        {
+            minimumValue >= arr[j] ? 0 : minimumValue = arr[j];
+        }
+        swapElements(arr, i, &minimumValue);
+        i++;
+    }
+}
+
 int main()
 {
     int valQuantity = 0, i = 0;
@@ -14,6 +37,8 @@ int main()
         numArr[i] = aux;
         i++;
     }
+
+    orderArr(numArr, valQuantity);
 
     delete[] numArr;
     return 0;
