6d6eeaa6eac6763e5f193a60117d43075c29a6cd
Pe-Guedss <pedro.og2002@gmail.com>
2022-05-24 11:48:55 -0300

    New tests for the cards.

--- tps/01-poker_face/TP/src/poker.cpp
+++ tps/01-poker_face/TP/src/poker.cpp
@@ -1,15 +1,21 @@
 #include "card.hpp"
-#include <iostream>
 
 int main()
 {
     Card *card1 = new Card("2A");
     Card *card2 = new Card("3A");
     Card *card3 = new Card("13A");
+    Card *card4 = new Card("13A");
 
     bool a = card2 < card1;
+    bool b = card2 > card1;
+    bool c = *card2 == *card3;
+    bool d = *card3 == *card4;
 
-    std::cout << a << std::endl;
+    std::cout << "card2 < card1 -> " << a << std::endl;
+    std::cout << "card2 > card1 -> " << b << std::endl;
+    std::cout << "card2 == card3 -> " << c << std::endl;
+    std::cout << "card3 == card4 -> " << d << std::endl;
 
     std::cout << card1 << std::endl;
     std::cout << *card2 << std::endl;
