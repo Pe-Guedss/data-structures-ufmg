017009903c4ee14085aae5bb28aa5fbf73a870a5
Pe-Guedss <pedro.og2002@gmail.com>
2022-05-25 09:01:34 -0300

    Creating the deck class.

---
+++ tps/01-poker_face/TP/include/deck.hpp
@@ -0,0 +1,16 @@
+//---------------------------------------------------------------------
+// Arquivo	: deck.hpp
+// Conteudo	: definicoes da classe Baralho
+// Autor	: Pedro de Oliveira Guedes (pedro.og2002@gmail.com)
+//---------------------------------------------------------------------
+
+#ifndef DECK_H
+#define DECK_H
+
+#include "card.hpp"
+
+class Deck {
+
+};
+
+#endif
