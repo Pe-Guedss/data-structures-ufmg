e6d1a98ad30d4fc270a1452e84319edfd7a8827c
Pe-Guedss <pedro.og2002@gmail.com>
2022-05-25 12:48:47 -0300

    Header file for the hand class.

---
+++ tps/01-poker_face/TP/include/hand.hpp
@@ -0,0 +1,14 @@
+//---------------------------------------------------------------------
+// Arquivo	: deck.hpp
+// Conteudo	: definicoes da classe Baralho
+// Autor	: Pedro de Oliveira Guedes (pedro.og2002@gmail.com)
+//---------------------------------------------------------------------
+
+#ifndef DECK_H
+#define DECK_H
+
+class hand {
+
+};
+
+#endif
