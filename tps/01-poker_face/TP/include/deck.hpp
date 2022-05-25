5d5ad44b70f3a4396bd26821aad7bd472b8a7500
Pe-Guedss <pedro.og2002@gmail.com>
2022-05-25 09:13:41 -0300

    Constructor e destructor methods.

--- tps/01-poker_face/TP/include/deck.hpp
+++ tps/01-poker_face/TP/include/deck.hpp
@@ -11,8 +11,28 @@
 #include <iostream>
 #include <string>
 
+/**
+ * @brief Classe que armazena todas as cartas do baralho, removendo-as para as mãos dos jogadores a cada nova rodada e restringindo que cartas repetidas sejam distribuídas.
+ * Há um baralho de 52 cartas em que as cartas estão organizadas de maneira crescente por número e naipes na seguinte sequência (O < E < C < P)
+ * 
+ */
 class Deck {
+    public:
+        /**
+         * @brief Construtor da classe deck, onde o baralho será inicializado com as cartas na ordem pré-definida.
+         * 
+         */
+        Deck();
+        /**
+         * @brief Destrutor da classe deck, todas as cartas serão deletadas.
+         * 
+         */
+        ~Deck();
 
+        
+
+    private:
+        Card *cardDeck[52];
 };
 
 #endif
