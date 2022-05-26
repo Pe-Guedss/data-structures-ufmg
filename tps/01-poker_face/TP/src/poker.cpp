7b94e9d29c498281313c2908b5572d5732a6770f
Pe-Guedss <pedro.og2002@gmail.com>
2022-05-25 21:39:35 -0300

    Testing the code strength.

--- tps/01-poker_face/TP/src/poker.cpp
+++ tps/01-poker_face/TP/src/poker.cpp
@@ -6,6 +6,7 @@ int main()
     cardDeck = new Deck();
 
     std::cout << cardDeck->getCard("13O") << std::endl;
+    std::cout << cardDeck->getCard("13O") << std::endl;
     std::cout << "\n\n==============================================\n\n";
     cardDeck->showDeck();
 
