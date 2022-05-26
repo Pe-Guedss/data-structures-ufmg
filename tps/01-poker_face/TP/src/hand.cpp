49630635381fe419d0b8b80b8ea4ceaff92e69eb
Pe-Guedss <pedro.og2002@gmail.com>
2022-05-25 23:14:41 -0300

    Implementing the sortHand method through selection sort.

--- tps/01-poker_face/TP/src/hand.cpp
+++ tps/01-poker_face/TP/src/hand.cpp
@@ -10,6 +10,20 @@ Hand::~Hand() {
     this->cleanHand();
 }
 
+void Hand::sortHand() {
+    for (int i = 0; i < this->maxCards; i++) {
+        Card *minCard = this->cards[i];
+        for (int j = i; j < this->maxCards; j++)
+        {
+            if (this->cards[j] < minCard)
+            {
+                minCard = this->cards[j];
+            }
+        }
+        swapCards(this->cards[i], minCard);
+    }
+}
+
 void Hand::cleanHand() {
     for (int i = 0; i < this->maxCards; i++)
     {
