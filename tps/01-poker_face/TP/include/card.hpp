d155ce69d59d99152838c96cb2531c064742852c
Pe-Guedss <pedro.og2002@gmail.com>
2022-05-25 23:13:30 -0300

    Defining a macro to swap two cards.

--- tps/01-poker_face/TP/include/card.hpp
+++ tps/01-poker_face/TP/include/card.hpp
@@ -10,6 +10,13 @@
 #include <iostream>
 #include <string>
 
+#define swapCards(A, B) \
+    {                      \
+        Card *aux = A;       \
+        A = B;             \
+        B = aux;           \
+    }
+
 /**
  * @brief Classe que armazena e gerencia as informacoes das cartas do baralho, enquanto objetos individuais.
  *
