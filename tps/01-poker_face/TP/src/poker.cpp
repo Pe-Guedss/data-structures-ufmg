9c159a6ed314e077519a0b509538072044882531
Pe-Guedss <pedro.og2002@gmail.com>
2022-05-29 20:10:07 -0300

    Testing the hand implementation.

--- tps/01-poker_face/TP/src/poker.cpp
+++ tps/01-poker_face/TP/src/poker.cpp
@@ -1,18 +1,36 @@
-#include "deck.hpp"
+#include "hand.hpp"
 
 int main()
 {
     Deck *cardDeck;
     cardDeck = new Deck();
 
-    std::cout << cardDeck->getCard("13O") << std::endl;
-    cardDeck->cleanDeck();
-    std::cout << "\n\n==============================================\n\n";
-    cardDeck->showDeck();
-    std::cout << "\n\n++++++++++++++++++++++++++++++++++++++++++++++++++\n\n";
-    cardDeck->initializeDeck();
-    cardDeck->showDeck();
+    Hand *firstHand;
+    firstHand = new Hand();
+    firstHand->addCard("2O", cardDeck);
+    firstHand->addCard("3E", cardDeck);
+    firstHand->addCard("4P", cardDeck);
+    firstHand->addCard("5C", cardDeck);
+    firstHand->addCard("6O", cardDeck);
 
-    delete cardDeck;
+    Hand *secondHand;
+    secondHand = new Hand();
+    secondHand->addCard("12C", cardDeck);
+    secondHand->addCard("1C", cardDeck);
+    secondHand->addCard("10C", cardDeck);
+    secondHand->addCard("11C", cardDeck);
+    secondHand->addCard("13O", cardDeck);
+
+    std::cout << "The first hand best combination is: " << firstHand->getBestCombinationCode() << std::endl;
+    std::cout << "The second hand best combination is: " << secondHand->getBestCombinationCode() << std::endl;
+
+    bool a = *firstHand > *secondHand;
+    bool b = *firstHand < *secondHand;
+    bool c = *firstHand == *secondHand;
+
+    std::cout << "Is the first Hand greater than the second one? " << a << std::endl;
+    std::cout << "Is the first Hand lesser than the second one? " << b << std::endl;
+    std::cout << "Is the first Hand equals the second one? " << c << std::endl;
+
     return 0;
 }
