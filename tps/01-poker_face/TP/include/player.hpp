f4c018126fea6769e10c5e700ee26252afb3d298
Pe-Guedss <pedro.og2002@gmail.com>
2022-05-31 23:23:07 -0300

    Novo construtor para o jogador.

--- tps/01-poker_face/TP/include/player.hpp
+++ tps/01-poker_face/TP/include/player.hpp
@@ -9,15 +9,34 @@
 
 #include "hand.hpp"
 
+/**
+ * @brief Classe Jogador. Guarda os atributos relacionados a ele, além de realizar operações como aposta.
+ * 
+ */
 class Player {
     public:
+        /**
+         * @brief Construtor padrão de um novo jogador.
+         * 
+         */
         Player();
+        /**
+         * @brief Construtor do jogador inicializando o nome do mesmo e o total de fichas iniciais que ele possui.
+         * 
+         * @param name Nome atribuído ao jogador.
+         * @param coins Total de fichas que o jogador possui inicialmente.
+         */
+        Player(std::string name, int coins);
+        /**
+         * @brief Destrutor padrão do jogador. Desaloca a mão e a torna um ponteiro nulo.
+         * 
+         */
         ~Player();
 
     private:
-        std::string name;
-        int coins;
-        Hand *hand;
+        std::string name; // Nome do jogador.
+        int coins; // Total de moedas do jogador.
+        Hand *hand; // Mão atual do jogador.
 
 };
 
