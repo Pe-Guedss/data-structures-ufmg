89c998b96e9f88128885c7dfb66839ca882eb31f
Pe-Guedss <pedro.og2002@gmail.com>
2022-06-01 11:56:40 -0300

    First player test.

--- tps/01-poker_face/TP/src/poker.cpp
+++ tps/01-poker_face/TP/src/poker.cpp
@@ -1,36 +1,56 @@
-#include "hand.hpp"
+#include <regex>
+#include "player.hpp"
 
-int main()
-{
+int main() {
     Deck *cardDeck;
     cardDeck = new Deck();
+    Player **players;
 
-    Hand *firstHand;
-    firstHand = new Hand();
-    firstHand->addCard("2O", cardDeck);
-    firstHand->addCard("3E", cardDeck);
-    firstHand->addCard("4P", cardDeck);
-    firstHand->addCard("5C", cardDeck);
-    firstHand->addCard("6O", cardDeck);
+    int playersAmount = 0, initialCoins = 1000, entryCoins = 0;
 
-    Hand *secondHand;
-    secondHand = new Hand();
-    secondHand->addCard("12C", cardDeck);
-    secondHand->addCard("1C", cardDeck);
-    secondHand->addCard("10C", cardDeck);
-    secondHand->addCard("11C", cardDeck);
-    secondHand->addCard("13O", cardDeck);
+    std::cin >> playersAmount;
+    players = new Player*[playersAmount];
+    std::cin >> entryCoins;
 
-    std::cout << "The first hand best combination is: " << firstHand->getBestCombinationCode() << std::endl;
-    std::cout << "The second hand best combination is: " << secondHand->getBestCombinationCode() << std::endl;
+    std::regex nameRegexp("[A-za-z]+");
+    std::regex betRegexp("[0-9]+");
+    std::regex cardRegexp("^[0-9]+[A-Za-z]");
+    for (int i = 0; i < playersAmount; i++) {
+        std::string aux = "", playerName = "";
+        std::cin >> aux;
+        // Reads the player name
+        while (std::regex_match(aux, nameRegexp)) {
+            playerName += aux + " ";
+            std::cin >> aux;
+        }
+        playerName[playerName.length() -1] = *"";
+        players[i] = new Player(playerName, initialCoins);
 
-    bool a = *firstHand > *secondHand;
-    bool b = *firstHand < *secondHand;
-    bool c = *firstHand == *secondHand;
+        if (!std::regex_match(aux, betRegexp)) {
+            std::cout << "A aposta não é válida!" << std::endl;
+            return 1;
+        }
+        if (!players[i]->sanityTest(entryCoins + atoi(aux.c_str()))) {
+            std::cout << "Jogador \"" << players[i]->getName() << "\" não possui fichas o suficiente." << std::endl;
+            return 1;
+        }
+        
+        for (int j = 0; j < 5; j++) {
+            std::cin >> aux;
+            if (std::regex_match(aux, cardRegexp)) {
+                players[i]->addCard(aux, cardDeck);
+            }
+        }
+    }
 
-    std::cout << "Is the first Hand greater than the second one? " << a << std::endl;
-    std::cout << "Is the first Hand lesser than the second one? " << b << std::endl;
-    std::cout << "Is the first Hand equals the second one? " << c << std::endl;
+    for (int i = 0; i < playersAmount; i++) {
+        std::cout << players[i] << std::endl;
+    }
+
+    for (int i = 0; i < playersAmount; i++) {
+        delete players[i];
+    }
+    delete []players;
 
     return 0;
 }
