195bbc5fe640bd1d77f8d3e49b75612989ee71e3
Pe-Guedss <pedro.og2002@gmail.com>
2022-05-31 23:08:05 -0300

    Starting the player class development.

---
+++ tps/01-poker_face/TP/include/player.hpp
@@ -0,0 +1,22 @@
+//---------------------------------------------------------------------
+// Arquivo	: player.hpp
+// Conteudo	: definicoes da classe Jogador
+// Autor	: Pedro de Oliveira Guedes (pedro.og2002@gmail.com)
+//---------------------------------------------------------------------
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "hand.hpp"
+
+class Player {
+    public:
+        Player();
+        ~Player();
+
+    private:
+        
+
+};
+
+#endif
