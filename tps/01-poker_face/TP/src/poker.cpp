347fe258d690bc4f25c328dea9e72326f0237481
Pe-Guedss <pedro.og2002@gmail.com>
2022-06-01 21:09:19 -0300

    Adding the pot logic.

--- tps/01-poker_face/TP/src/poker.cpp
+++ tps/01-poker_face/TP/src/poker.cpp
@@ -25,18 +25,18 @@ int main() {
     cardDeck = new Deck();
 
     Player **players;
-
-    int playersAmount = 0, initialCoins = 1000, entryCoins = 0;
+    int pot = 0;
+    int playersAmount = 0, initialCoins = 1000, openingBet = 0;
 
     std::cin >> playersAmount;
     players = new Player*[playersAmount];
-    std::cin >> entryCoins;
+    std::cin >> openingBet;
 
     std::regex betRegexp("[0-9]+");
     std::regex cardRegexp("^[0-9]+[A-Za-z]");
     for (int i = 0; i < playersAmount; i++) {
         std::string aux = "";
-        
+
         std::string currentPlayerName = readName(aux);
         players[i] = new Player(currentPlayerName, initialCoins);
 
@@ -44,11 +44,12 @@ int main() {
             std::cout << "A aposta não é válida!" << std::endl;
             return 1;
         }
-        int currentPlayerBet = entryCoins + atoi(aux.c_str());
+        int currentPlayerBet = openingBet + atoi(aux.c_str());
         if (!players[i]->sanityTest(currentPlayerBet)) {
             std::cout << "Jogador \"" << players[i]->getName() << "\" não possui fichas o suficiente." << std::endl;
             return 1;
         }
+        pot += currentPlayerBet;
         betsQueue->enqueue(currentPlayerBet);
         
         for (int j = 0; j < 5; j++) {
@@ -59,6 +60,7 @@ int main() {
         }
     }
 
+    std::cout << "The pot currently is: " << pot << std::endl;
     for (int i = 0; i < playersAmount; i++) {
         players[i]->makeBet(betsQueue->dequeue());
         std::cout << players[i] << std::endl;
