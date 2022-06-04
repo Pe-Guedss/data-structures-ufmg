413cc81ed2b0a157080ff81f3f1c557e5c932cfc
Pe-Guedss <pedro.og2002@gmail.com>
2022-06-04 00:25:51 -0300

    Finishing the test file.

--- tps/01-poker_face/TP/src/poker.cpp
+++ tps/01-poker_face/TP/src/poker.cpp
@@ -1,78 +1,11 @@
 #include <regex>
-#include "round.hpp"
-
-std::string readName(std::string &aux) {
-    std::regex nameRegexp("[A-za-z]+");
-    std::string playerName = "";
-
-    std::cin >> aux;
-    // Reads the player name
-    while (std::regex_match(aux, nameRegexp)) {
-        playerName += aux + " ";
-        std::cin >> aux;
-    }
-    playerName[playerName.length() -1] = *"";
-
-    return playerName;
-}
-
-int readBet(std::string &aux) {
-    std::regex betRegexp("[0-9]+");
-
-    if (!std::regex_match(aux, betRegexp)) {
-        std::cout << "A aposta não é válida!" << std::endl;
-        return 1;
-    }
-
-    return atoi(aux.c_str());
-}
+#include "match.hpp"
 
 int main() {
-    Player **players;
-    int playersAmount = 0, initialCoins = 1000, openingBet = 0;
+    Match *match;
+    match = new Match("entrada.txt");
 
-
-    Round *round;
-
-    std::cin >> playersAmount;
-    players = new Player*[playersAmount];
-    std::cin >> openingBet;
-
-    round = new Round(playersAmount, openingBet);
-
-    std::regex cardRegexp("^[0-9]+[A-Za-z]");
-    for (int i = 0; i < playersAmount; i++) {
-        std::string aux = "";
-
-        std::string currentPlayerName = readName(aux);
-        players[i] = new Player(currentPlayerName, initialCoins);
-
-        int currentPlayerBet = readBet(aux);
-        if (!players[i]->sanityTest(currentPlayerBet + openingBet)) {
-            std::cout << "Jogador \"" << players[i]->getName() << "\" não possui fichas o suficiente." << std::endl;
-            round->invalidateRound();
-        }
-
-        round->pushBet(currentPlayerBet);
-        round->addPlayer(i, players[i]);
-        
-        for (int j = 0; j < 5; j++) {
-            std::cin >> aux;
-            if (std::regex_match(aux, cardRegexp)) {
-                round->pushCardToPlayer(i, aux);
-            }
-        }
-        
-        round->checkPlayerHand(i);
-    }
-
-    if (round->isRoundValid()) {
-
-        round->demandOpeningBet();
-        round->collectBets();
-        round->transferPotCoinsToWinners();
-        round->printRoundInfo();
-    }
+    match->play("saida.txt");
 
     return 0;
 }
