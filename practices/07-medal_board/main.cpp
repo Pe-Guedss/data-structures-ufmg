31d6ba022d6d633ca7eb1842499bf20f564a5468
Pe-Guedss <pedro.og2002@gmail.com>
2022-06-14 15:01:28 -0300

    Creating the class Country and defining the less than operator.

---
+++ practices/07-medal_board/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+
+class Country {
+    public:
+        bool operator < (Country const country) {
+            if (this->goldMedals < country.goldMedals) return true;
+            else if (this->goldMedals > country.goldMedals) return false;
+
+            if (this->silverMedals < country.silverMedals) return true;
+            else if (this->silverMedals > country.silverMedals) return false;
+
+            if (this->bronzeMedals < country.bronzeMedals) return true;
+            else if (this->bronzeMedals > country.bronzeMedals) return false;
+
+            return false;
+        }
+
+        // bool operator > (Country const &country) {
+        //     return this->number > country.number;
+        // }
+
+    private:
+        std::string name;
+        int goldMedals;
+        int silverMedals;
+        int bronzeMedals;
+};
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+ 
+    return 0;
+}
