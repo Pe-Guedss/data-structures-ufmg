932975a42138b453baccce944c4abf8112ae0b98
Pe-Guedss <pedro.og2002@gmail.com>
2022-06-26 19:12:53 -0300

    LexOrder class implementation file.

---
+++ tps/02-phrase_sorter/TP/src/lex-order.cpp
@@ -0,0 +1,13 @@
+#include "lex-order.hpp"
+
+LexOrder::LexOrder(std::string LexOrder) {
+
+}
+
+int LexOrder::getLetterValue(std::string letter) {
+
+}
+
+bool LexOrder::isLessThan(std::string a, std::string b) {
+
+}
