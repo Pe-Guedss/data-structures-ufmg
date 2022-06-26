443a49e84e8c2830a82d569e9b0a1ce461c5cf30
Pe-Guedss <pedro.og2002@gmail.com>
2022-06-26 19:22:03 -0300

    Adding the setLetterValue to the LexOrder class implementations file.

--- tps/02-phrase_sorter/TP/src/lex-order.cpp
+++ tps/02-phrase_sorter/TP/src/lex-order.cpp
@@ -8,6 +8,10 @@ int LexOrder::getLetterValue(std::string letter) {
 
 }
 
+void LexOrder::setLetterValue(std::string letter, int value) {
+
+}
+
 bool LexOrder::isLessThan(std::string a, std::string b) {
 
 }
