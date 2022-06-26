88e1e59b3a808a8439b2f585c563faf3bfe01466
Pe-Guedss <pedro.og2002@gmail.com>
2022-06-26 19:09:55 -0300

    Documenting the lex-order methods.

--- tps/02-phrase_sorter/TP/include/lex-order.hpp
+++ tps/02-phrase_sorter/TP/include/lex-order.hpp
@@ -10,15 +10,41 @@
 #include <iostream>
 #include <string>
 
+/**
+ * @brief Classe LexOrder, define a ordem lexicográfica com a qual as palavras vão ser ordenadas.
+ * 
+ */
 class LexOrder {
     public:
+        /**
+         * @brief Construtor da classe LexOrder. Atribui valor a todas as letras, crescentemente de acordo com a string recebida em lexOrder.
+         * 
+         * @param lexOrder String com a ordem lexicográfica a ser seguida.
+         */
         LexOrder(std::string lexOrder);
 
+        /**
+         * @brief Retorna o valor da letra recebida como parâmetro.
+         * 
+         * @param letter Letra que se quer saber o valor na nova ordem lexicográfica.
+         * @return int Valor da letra na ordem lexicográfica atual.
+         */
         int getLetterValue(std::string letter);
 
+        /**
+         * @brief Método para definir, de acordo com a nova ordem lexicográfica, se a letra recebida em "a" é menor que a recebida em "b".
+         * 
+         * @param a Letra do alfabeto
+         * @param b Letra do alfabeto
+         * @return true (1) ou false (0), respectivamente se a < b ou não.
+         */
         bool isLessThan(std::string a, std::string b);
 
     private:
+        /**
+         * @brief Letra do alfabeto.
+         * 
+         */
         int a = -1, b = -1, c = -1, d = -1, e = -1,\
             f = -1, g = -1, h = -1, i = -1, j = -1, \
             k = -1, l = -1, m = -1, n = -1, o = -1,\
