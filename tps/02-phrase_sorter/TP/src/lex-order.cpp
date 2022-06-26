b6d5a4857298f01669c74e3e1e43dc0d1c11d2ca
Pe-Guedss <pedro.og2002@gmail.com>
2022-06-26 19:44:21 -0300

    Implementing the setLetterValue method.

--- tps/02-phrase_sorter/TP/src/lex-order.cpp
+++ tps/02-phrase_sorter/TP/src/lex-order.cpp
@@ -13,11 +13,36 @@ LexOrder::LexOrder(std::string lexOrder) {
     
 }
 
-int LexOrder::getLetterValue(std::string letter) {
-
+void LexOrder::setLetterValue(std::string letter, int value) {
+    if (letter == "a") this->a = value;
+    else if (letter == "b") this->b = value;
+    else if (letter == "c") this->c = value;
+    else if (letter == "d") this->d = value;
+    else if (letter == "e") this->e = value;
+    else if (letter == "f") this->f = value;
+    else if (letter == "g") this->g = value;
+    else if (letter == "h") this->h = value;
+    else if (letter == "i") this->i = value;
+    else if (letter == "j") this->j = value;
+    else if (letter == "k") this->k = value;
+    else if (letter == "l") this->l = value;
+    else if (letter == "m") this->m = value;
+    else if (letter == "n") this->n = value;
+    else if (letter == "o") this->o = value;
+    else if (letter == "p") this->p = value;
+    else if (letter == "q") this->q = value;
+    else if (letter == "r") this->r = value;
+    else if (letter == "s") this->s = value;
+    else if (letter == "t") this->t = value;
+    else if (letter == "u") this->u = value;
+    else if (letter == "v") this->v = value;
+    else if (letter == "w") this->w = value;
+    else if (letter == "x") this->x = value;
+    else if (letter == "y") this->y = value;
+    else if (letter == "z") this->z = value;
 }
 
-void LexOrder::setLetterValue(std::string letter, int value) {
+int LexOrder::getLetterValue(std::string letter) {
 
 }
 
