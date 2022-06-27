e9960ce5288e8839dabb13c5e6ecf64d3d80f17c
Pe-Guedss <pedro.og2002@gmail.com>
2022-06-26 22:58:17 -0300

    Moving the "toLower" method to the LexOrder class.

--- tps/02-phrase_sorter/TP/src/main.cpp
+++ tps/02-phrase_sorter/TP/src/main.cpp
@@ -2,34 +2,20 @@
 #include <sstream>
 #include <iostream>
 
-std::string toLower(std::string str) {
-    const uint diff = 'a' - 'A';
-
-    std::string lower;
-    lower.reserve(str.length());
-
-    for (int i = 0; i < str.size(); i++) {
-        lower += (str[i] >= 'A' && str[i] <= 'Z') ? str[i] + diff : str[i];
-    }
-
-    return lower;
-}
-
 int main() {
     std::string aaa;
     LexOrder *lexOrder;
 
     std::getline(std::cin, aaa);
-    aaa = toLower(aaa);
     lexOrder = new LexOrder(aaa);
 
     std::string test[3];
-    test[0] = "Era", test[1] = "uma", test[2] = "vez";
+    test[0] = "Era", test[1] = "uma", test[2] = "umas";
     for (int i = 0; i < 3; i++) {
         std::string aux = "";
         int minStr = i;
         for (int j = i; j < 3; j++) {
-            if ( lexOrder->isLessThan(test[j], test[i]) ) {
+            if ( lexOrder->isLessThan(test[j], test[minStr]) ) {
                 minStr = j;
             }
         }
