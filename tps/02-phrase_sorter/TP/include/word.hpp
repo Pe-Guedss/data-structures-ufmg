ed23671b8018f6956f22fb06d0b7488f217327f8
Pe-Guedss <pedro.og2002@gmail.com>
2022-06-26 23:18:25 -0300

    Documenting the Word class methods.

--- tps/02-phrase_sorter/TP/include/word.hpp
+++ tps/02-phrase_sorter/TP/include/word.hpp
@@ -9,17 +9,41 @@
 
 #include "lex_order.hpp"
 
+/**
+ * @brief Classe Word. Define os atributos que devem ser guardados para a contagem e ordenação das palavras.
+ * 
+ */
 class Word {
     public:
+        /**
+         * @brief Construtor da classe Word. Atribui a string recebida à palavra que o objeto armazena.
+         * 
+         * @param word Palavra que se quer armazenar.
+         */
         Word(std::string word);
 
-        void matches(std::string newWord);
+        /**
+         * @brief Verifica se a string recebida é igual à palavra que este objeto armazena.
+         * 
+         * @param newWord Palavra que se quer verificar a igualdade.
+         * 
+         * @return true(1) se a string é igual à palavra armazenada e false (0) caso contrário
+         */
+        bool matches(std::string newWord);
 
+        /**
+         * @brief Verifica se o objeto de palavra atual é menor que o fornecido por parâmetro.
+         * 
+         * @param word Objeto que se quer saber se é maior que o objeto atual.
+         * @param lexOrder Ordem lexicográfica na qual os objetos serão comparados.
+         * 
+         * @return true (1) se o objeto palavra atual for menor e false (0) caso contrário.
+         */
         bool isLessThan(Word *word, LexOrder *lexOrder);
 
     private:
-        std::string word;
-        int occurrences;
+        std::string word; // Palavra armazenada pela classe.
+        int occurrences; // Quantidade de vezes que essa palavra foi lida.
 };
 
 #endif
