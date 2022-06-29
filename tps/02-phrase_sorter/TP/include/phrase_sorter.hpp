f787133deddb66cbdffa390cda0fa20b964f0874
Pe-Guedss <pedro.og2002@gmail.com>
2022-06-29 00:10:44 -0300

    Changing the median attribute purpose.

--- tps/02-phrase_sorter/TP/include/phrase_sorter.hpp
+++ tps/02-phrase_sorter/TP/include/phrase_sorter.hpp
@@ -43,7 +43,7 @@ class PhraseSorter {
         WordList *wordList; // Lista encadeada inicial das palavras lidas.
         Word **words; // Array dinâmico de palavras para ser ordenado.
 
-        int median; // Quantidade de elementos levados em consideração na heurística de mediana no quick sort.
+        std::string medianWord; // Palavra que deve ser considerada como a mediana para a heurística do Quick Sort.
         Word **medianArr; // Array com a quantidade this->median de elementos para que o cálculo seja feito.
 
         int size; // Tamanho do array de palavras.
