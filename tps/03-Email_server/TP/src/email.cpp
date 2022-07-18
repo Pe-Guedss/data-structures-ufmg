cd014eab6d5f1d114eb8fb5bbab8ecd0447aa74f
Pe-Guedss <pedro.og2002@gmail.com>
2022-07-18 15:49:35 -0300

    Defining every Email class method.

---
+++ tps/03-Email_server/TP/src/email.cpp
@@ -0,0 +1,55 @@
+#include "email.hpp"
+
+Email::Email() {
+    this->addressee = -1;
+    this->id = -1;
+
+    this->content = EmailContent();
+
+    this->parent = nullptr;
+    this->leftEmail = nullptr;
+    this->rightEmail = nullptr;
+}
+
+Email::Email(int emailID, int userID) {
+    this->addressee = userID;
+    this->id = emailID;
+
+    this->content = EmailContent();
+
+    this->parent = nullptr;
+    this->leftEmail = nullptr;
+    this->rightEmail = nullptr;
+}
+
+void Email::setParent(Email *parent) {
+    this->parent = parent;
+}
+
+void Email::setLeftEmail(Email *leftEmail) {
+    this->leftEmail = leftEmail;
+}
+
+void Email::setRightEmail(Email *rightEmail) {
+    this->rightEmail = rightEmail;
+}
+
+void Email::setContent(int wordsAmount, std::string msg) {
+    this->content = EmailContent(wordsAmount, msg);
+}
+
+int Email::getID() {
+    return this->id;
+}
+
+int Email::getAdressee() {
+    return this->addressee;
+}
+
+std::ostream &operator<<(std::ostream &out, const Email *email) {
+    out << "O ID deste e-mail é: " << email->id << std::endl;
+    out << "Ele foi recebido pelo usuário de ID: " << email->addressee << std::endl;
+    out << &email->content;
+    
+    return out;
+}
