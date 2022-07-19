23bf473876be36ec786df6533355ec2d195cba34
Pe-Guedss <pedro.og2002@gmail.com>
2022-07-19 01:39:56 -0300

    Testing the Session class.

--- tps/03-Email_server/TP/src/main.cpp
+++ tps/03-Email_server/TP/src/main.cpp
@@ -1,24 +1,10 @@
-#include "server.hpp"
+#include "session.hpp"
 
 int main() {
-    Server *server;
-    server = new Server(5);
+    Session *session;
+    session = new Session("in.txt");
 
-    server->sendEmail(10, 1, 5, "Olá, eu sou Pedro! Prazer!");
-    server->sendEmail(17, 12, 5, "Olá, eu sou Vinícius! Prazer!");
-    server->sendEmail(1, 5, 5, "Olá, eu sou Carlos! Prazer!");
-    server->sendEmail(5, 85, 5, "Olá, eu sou João! Prazer!");
-    server->sendEmail(19, 9, 5, "Olá, eu sou Outro! Prazer!");
-
-    server->searchEmail(5, 85);
-    server->searchEmail(10, 85);
-    server->searchEmail(1, 5);
-
-    server->eraseEmail(19, 9);
-
-    OperationsQueue *log;
-    log = server->getServerLog();
-    while (log->getSize() > 0) std::cout << log->dequeue() << std::endl;
+    session->finish("out.txt");
     
     return 0;
 }
